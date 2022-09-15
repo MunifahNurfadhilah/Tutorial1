2f7ae4e7d5153e4f73e111cc0b0ec0c2b825d4ae
munifah.nurfadhilah <munifah.nurfadhilah@ui.ac.id>
2022-09-15 15:15:31 +0700

    json xml

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
@@ -10,3 +12,19 @@ def show_wishlist(request):
     'nama': 'pudil'
     }
     return render(request, "wishlist.html", context)
+    
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
